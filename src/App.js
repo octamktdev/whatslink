840dba316cd00e007342cadb443a8929a807a2fb
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-06-17 17:27:15 -0300

    chore: add react-icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, {useState} from 'react';
-import './App.css';
 
 /*
 Regra geral: 
 - O texto da mensagem não pode conter espaço em branco, sendo substituído por %20
 - O número deve conter o CountryCode+DDD+numero = 5513988282873
 - Link geral: https://api.whatsapp.com/send?phone=55${phone}&text=${message}
+- Encurtar link : https://dev.bitly.com/
 */
 
-/*
-Colors
-Teal Green
-#075E54 (dark)
-#128C7E (light)
-
-Light Green
-#25D366
-
-Checkmark Blue
-#34B7F1
-*/
 
 function App() {
   const [phone, setPhone] = useState('')
@@ -28,7 +16,7 @@ function App() {
   let urlWhatsappLink = `https://api.whatsapp.com/send?phone=55${phone}&text=${message}`
 
   return (
-    <main>
+    <main className="page-home">
 
     </main>
   );
